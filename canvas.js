4982b918bfea4562b3af8f85f3624d8b353a4359
Novan <l4novan132@gmail.com>
2023-07-22 15:32:24 +0700

    add logic to simulate gravity and fraction to the balls

--- canvas.js
+++ canvas.js
@@ -16,15 +16,22 @@ let colors = [
     "#ff7f66"
 ]
 
-addEventListerner("mousemove", function(event) {
+const gravity = 1;
+const friction = 0.99;
+
+addEventListener("mousemove", function(event) {
     mouse.x = event.clientX;
     mouse.y = event.clientY;
 });
 
-addEventListerner("resize", function() {
+addEventListener("resize", function() {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
 
+    init();
+});
+
+addEventListener("click", function() {
     init();
 });
 
@@ -36,13 +43,28 @@ function randomColor(colors) {
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function Ball(x, y, radius, color) {
+function Ball(x, y, dx, dy, radius, color) {
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
     this.radius = radius;
     this.color = color;
 
     this.update = function() {
+        if (this.y + this.radius + this.dy > canvas.height) {
+            this.dy = -this.dy * friction;
+        }
+        else {
+            this.dy += gravity;
+        }
+
+        if (this.x + this.radius + this.dx > canvas.width || this.x + this.dx < this.radius) {
+            this.dx = -this.dx;
+        }
+
+        this.x += this.dx;
+        this.y += this.dy;
         this.draw();
     }
 
@@ -51,16 +73,33 @@ function Ball(x, y, radius, color) {
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
+        c.stroke();
         c.closePath();
     }
 }
 
-function init() {}
+let ball;
+let ballArray = [];
+
+function init() {
+    ballArray = [];
+    for (let i = 0; i < 300; ++i) {
+        let radius = randomIntFromRange(8, 20);
+        let x = (randomIntFromRange(0, canvas.width - 2 * radius) + radius);
+        let y = (randomIntFromRange(0, canvas.height - 2 * radius) + radius);
+        let dx = randomIntFromRange(-2, 2);
+        let dy = randomIntFromRange(-2, 2);
+        let color = randomColor(colors);
+        ballArray.push(new Ball(x, y, dx, dy, radius, color));
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
-    c.fillText("boiler text", mouse.x, mouse.y);
+    for (let i = 0; i < ballArray.length; ++i) {
+        ballArray[i].update();
+    }
 }
 
 init();
